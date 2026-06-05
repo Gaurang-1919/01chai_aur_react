5838552aa4be149ca4f9e56015f18b7a44414876
Gaurang-1919 <gaurang.ug24@nsut.ac.in>
2026-06-05 23:29:34 +0530

    Interview

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObject = {
+    username:"gaurang",
+    age: 20
+  }
+
+  let newArr = [1,3,65,8,0]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      <Card channel = "chaiaurcode" someObj = {newArr} />
+      <Card />
+   </>
+  )
+}
+
+export default App
